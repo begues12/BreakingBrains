d69aac3531b07e0d73b66a964df7dde3875029c2
begues12 <beeguespark@gmail.com>
2024-09-30 22:14:42 +0200

    Cambios más generales

--- MVC/Controllers/OurDjs/Index.php
+++ MVC/Controllers/OurDjs/Index.php
@@ -5,19 +5,32 @@ use Engine\Core\IController;
 
 class Index extends IController
 {
-    private $photos;
-    private $photo_dir;
 
     function __construct()
     {
         parent::__construct();
-        $this->photo_dir = 'Assets/Images/TeamPhoto';
     }
 
     public function prepare()
     {
-        // Get all photos from Assets\Images\TeamPhoto
-        $this->setVar('photos', $this->getPhotos());
+        
+        $this->setVar('biel-text',[
+            'src'           => 'Assets/Images/TeamPhoto/IMG_3489.JPG',
+            'name'          => 'Biel',
+            'description'   => 'Lo que realmente me apasiona del mundo del DJ es la capacidad de romper con lo convencional y sorprender al público con mezclas innovadoras y emocionantes. Creo firmemente que la música no debe ser plana ni predecible; por eso, siempre busco crear sets que destaquen y ofrezcan una experiencia única en cada presentación. Aunque mi especialidad es el tech house, disfruto explorando y fusionando diferentes géneros, lo que me permite adaptarme a cualquier estilo musical según el ambiente y la energía del público. Me considero un perfeccionista, y cada vez que me pongo detrás de los platos, intento superar mis propios límites, buscando mejorar y evolucionar constantemente en mi técnica y creatividad.',
+        ]);
+
+        $this->setVar('ntk-text',[
+            'src'           => 'Assets\Images\TeamPhoto\FuentesTheLake_SergiLopez 3.jpg',
+            'name'          => 'NTK',
+            'description'   => 'La música siempre ha formado gran parte de mi vida, me encanta llegar a crear sonidos especiales, con gran capacidad de adaptación entre estilos, mis sessiones no se componen de un genero unico, ya que me gusta mezclar cambios entre generos generando un ambierte unico y especial, siempre intento que el publico se sienta identificado con la musica que pongo, y que disfruten de la experiencia de la musica en directo.',
+        ]);
+
+        $this->setVar('sadi-text', [
+            'src'           => 'Assets\Images\TeamPhoto\DSC_0010.JPG',
+            'name'          => 'Sadi',
+            'description'   => 'Para mí, ser DJ es mucho más que mezclar canciones; es crear una atmósfera única y conectar con la energía de la gente. Lo que más disfruto es llevar al público en un viaje sonoro que los haga sentir, bailar y olvidar el mundo exterior. Mi estilo se mueve entre el deep house y el techno melódico, siempre buscando esos ritmos que resuenan en el alma y hacen vibrar el cuerpo. Cada set es una nueva oportunidad para experimentar y fusionar sonidos, manteniendo siempre un equilibrio entre lo familiar y lo inesperado. Mi objetivo es que cada presentación sea inolvidable, y para lograrlo, trabajo constantemente en perfeccionar mi técnica y ampliar mis conocimientos musicales. Soy un firme creyente en la evolución, tanto en la música como en mi carrera, y eso me impulsa a desafiarme en cada actuación.'
+        ]);
 
     }
 
@@ -27,21 +40,6 @@ class Index extends IController
 
     public function finish()
     {   
-    }
-
-    public function getPhotos()
-    {
-        $this->photos = scandir($this->photo_dir);
-
-        foreach($this->photos as $key => $photo)
-        {
-            if($photo == '.' || $photo == '..')
-            {
-                unset($this->photos[$key]);
-            }
-        }
-
-        return $this->photos;
     }
 
 }
--- MVC/Views/OurDjs/Index.php
+++ MVC/Views/OurDjs/Index.php
@@ -8,80 +8,70 @@ use Engine\Core\HTML;
 
 class Index extends IView
 {
-    private $photos;
-    private $div_gallery;
+    private $djs;
+    private $div_djs;
 
     public function prepare()
     {
         $this->setHeader(new Header());
         // Aquí deberías cargar los datos de los DJs de una base de datos o configuración
-        $this->photos = [
-            ['src' => 'Assets\Images\TeamPhoto\SadiTheLake_SergiLopez 1.jpg', 'name' => 'OMEGA', 'description' => 'Specializing in house and electronic beats.'],
-            ['src' => 'Assets\Images\TeamPhoto\SadiTheLake_SergiLopez 1.jpg', 'name' => 'SADI', 'description' => 'Specializing in house and electronic beats.'],
-            ['src' => 'Assets\Images\TeamPhoto\SadiTheLake_SergiLopez 1.jpg', 'name' => 'NTK', 'description' => 'Bringing the best of hip hop and top 40.'],
-            // Agrega más DJs aquí...
+        $this->djs = [
+            $this->getVar('biel-text'),
+            $this->getVar('ntk-text'),
+            $this->getVar('sadi-text')
         ];
     }
 
     public function createObjects()
     {
         // Contenedor principal de la galería
-        $this->div_gallery = new HTML('div', ['class' => 'container']);
-        $this->div_gallery->setClasses(['row', 'justify-content-center', 'g-4', 'w-100']);
-        $this->div_gallery->setStyle([
-            'margin-top' => '50px',
-            'margin-bottom' => '50px',
-            'margin-left' => 'auto',
-            'margin-right' => 'auto'
-        ]);
+        $this->div_djs = new HTML('div', ['class' => 'container']);
+        $this->div_djs->setClasses(['djs-gallery']);
 
         $this->createPhotoGallery();
     }
 
     public function compile()
     {
-        $this->addBody($this->div_gallery);
+        $this->addBody($this->div_djs);
     }
 
     private function createPhotoGallery()
     {
-        foreach ($this->photos as $photo) {
-            $this->createPhotoCard($photo);
+        foreach ($this->djs as $dj) {
+            $this->createDjCard($dj);
         }
     }
 
-    private function createPhotoCard($dj)
+    private function createDjCard($dj)
     {
-        $div_card = new HTML('div', ['class' => 'col-lg-3 col-md-4 col-sm-6 my-3 card-dj']);
+        // Tarjeta contenedora con flexbox para la estructura responsive
+        $div_card = new HTML('div', ['class' => 'dj-card']);
 
-        // Imagen
+        $div_img = new HTML('div', ['class' => 'dj-img']);
+
         $img = new HTML('img', [
             'src' => $dj['src'],
             'class' => 'img-fluid',
             'alt' => 'Photo of ' . $dj['name']
         ]);
-        $img->setStyle([
-            'width' => '100%',
-            'height' => 'auto',
-            'border-radius' => '8px'
-        ]);
 
-        // Nombre del DJ
-        $h5 = new HTML('h5');
+        $div_text = new HTML('div', ['class' => 'dj-info']);
+
+        $h5 = new HTML('h4');
         $h5->setText($dj['name']);
-        $h5->setStyle(['color' => '#fff', 'text-align' => 'center', 'margin-top' => '15px']);
 
-        // Descripción del DJ
         $p = new HTML('p');
         $p->setText($dj['description']);
-        $p->setStyle(['color' => '#ccc', 'text-align' => 'center', 'font-size' => '14px']);
 
-        $div_card->addElement($img);
-        $div_card->addElement($h5);
-        $div_card->addElement($p);
+        $div_text->addElement($h5);
+        $div_text->addElement($p);
 
-        $this->div_gallery->addElement($div_card);
+        $div_img->addElement($img);
+
+        $div_card->addElement($div_img);
+        $div_card->addElement($div_text);
+
+        $this->div_djs->addElement($div_card);
     }
 }
-
-?>
