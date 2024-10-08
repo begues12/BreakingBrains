cf7afaa63e53bcb1699bfa815a569ff500b7998c
begues12 <beeguespark@gmail.com>
2024-10-08 18:17:28 +0200

    awad

--- MVC/Views/Events/Index.php
+++ MVC/Views/Events/Index.php
@@ -22,7 +22,7 @@ class Index extends IView
     {
         // Contenedor principal de la galería
         $this->div_gallery = new HTML('div', ['class' => 'container']);
-        $this->div_gallery->setClasses(['row', 'justify-content-center', 'g-4', 'w-100']);
+        $this->div_gallery->setClasses(['row', 'justify-content-between', 'g-4', 'w-100']);
         $this->div_gallery->setStyle([
             'margin-top'    => '50px',
             'margin-bottom' => '50px',
@@ -49,29 +49,90 @@ class Index extends IView
 
     private function createEventCard($event, $isPast)
     {
-        $div_card = new HTML('div', ['class' => 'col-lg-3 col-md-6 col-sm-12 m-3 p-0 card-event']);
+        $div_card = new HTML('div', ['class' => 'col-lg-4 col-md-4 col-sm-12 m-3 p-0 card-event']);
         $div_card->setStyle([
             'margin-left' => 'auto',
             'margin-right' => 'auto',
             'overflow' => 'hidden'
         ]);
 
-        // Imagen del evento
-        $img = new HTML('img', [
-            'src' => $event['image'],
-            'class' => 'img-fluid',
-            'alt' => 'Photo of event ' . $event['title']
+        // Carrusel de medios (imágenes y videos)
+        $carousel_id = 'carousel_' . uniqid(); // ID único para cada carrusel
+        $div_carousel = new HTML('div', [
+            'id' => $carousel_id,
+            'class' => 'carousel slide',
+            'data-bs-ride' => 'carousel'
         ]);
-        $img->setStyle([
-            'width' => '100%',
-            'height' => '200px',
+        $div_carousel_inner = new HTML('div', ['class' => 'carousel-inner']);
+
+        foreach ($event['media'] as $index => $media) {
+            $carousel_item = new HTML('div', [
+                'class' => $index === 0 ? 'carousel-item active' : 'carousel-item'
+            ]);
+
+            if (preg_match('/\.(jpg|jpeg|png|gif)$/i', $media)) {
+                // Si el archivo es una imagen
+                $img = new HTML('img', [
+                    'src' => $media,
+                    'class' => 'd-block w-100',
+                    'alt' => 'Media for event ' . $event['title']
+                ]);
+                $img->setStyle([
+                    'height' => '180px',
+                    'object-fit' => 'cover' // Mantener el ratio de la imagen
+                ]);
+                $carousel_item->addElement($img);
+            } elseif (preg_match('/\.(mp4|webm)$/i', $media)) {
+                // Si el archivo es un video
+                $video = new HTML('video', [
+                    'class' => 'd-block w-100',
+                    'controls' => true
+                ]);
+                $source = new HTML('source', [
+                    'src' => $media,
+                    'type' => 'video/mp4'
+                ]);
+                $video->addElement($source);
+                $video->setStyle([
+                    'height' => '180px',
+                    'object-fit' => 'cover' // Mantener el ratio de la imagen del video
+                ]);
+                $carousel_item->addElement($video);
+            }
+
+            $div_carousel_inner->addElement($carousel_item);
+        }
+
+        $div_carousel->addElement($div_carousel_inner);
+
+        // Controles del carrusel (prev y next)
+        $carousel_control_prev = new HTML('a', [
+            'class' => 'carousel-control-prev',
+            'href' => '#' . $carousel_id,
+            'role' => 'button',
+            'data-bs-slide' => 'prev'
         ]);
+        $carousel_control_prev->addElement(new HTML('span', ['class' => 'carousel-control-prev-icon']));
+        
+        $carousel_control_next = new HTML('a', [
+            'class' => 'carousel-control-next',
+            'href' => '#' . $carousel_id,
+            'role' => 'button',
+            'data-bs-slide' => 'next'
+        ]);
+        $carousel_control_next->addElement(new HTML('span', ['class' => 'carousel-control-next-icon']));
+        
+        $div_carousel->addElement($carousel_control_prev);
+        $div_carousel->addElement($carousel_control_next);
+
+        // Añadir el carrusel al div de la tarjeta
+        $div_card->addElement($div_carousel);
 
         // Nombre del evento
         $h5 = new HTML('h5');
         $h5->setText($event['title'] . ($isPast ? " - Finalizado" : ""));
         $h5->setStyle(['color' => '#fff', 'text-align' => 'center', 'margin-top' => '15px', 'font-size' => '20px']);
-        $h5->setClass( $isPast ? 'bg-danger' : 'bg-primary' );
+        $h5->setClass($isPast ? 'bg-danger' : 'bg-primary');
 
         $h5_2 = new HTML('h5');
         $h5_2->setText($event['date']);
@@ -82,11 +143,12 @@ class Index extends IView
         $p->setText($event['description']);
         $p->setStyle(['color' => '#ccc', 'text-align' => 'center', 'font-size' => '14px']);
 
-        $div_card->addElement($img);
         $div_card->addElement($h5);
         $div_card->addElement($h5_2);
         $div_card->addElement($p);
 
         $this->div_gallery->addElement($div_card);
     }
+
+    
 }
