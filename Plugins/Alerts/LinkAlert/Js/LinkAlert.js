a4b7f9b80331a001e40f89ef0c15e3779cf68bb1
Alejandro Fuentes <afuentes@globalholdings2000.es>
2024-10-09 16:52:40 +0200

    dwas

--- Plugins/Alerts/LinkAlert/Js/LinkAlert.js
+++ Plugins/Alerts/LinkAlert/Js/LinkAlert.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    $(".basic-alert").animate({left: '150%'}, "slow");
     // A los 5 segundos de haber cargado la página, cierra la alerta de éxito
     setTimeout(function () {
         // FadeOut to basic alert class
