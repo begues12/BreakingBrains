f383615082caa9947c32fdfa2f57190003aad325
Alejandro Fuentes <afuentes@globalholdings2000.es>
2024-10-10 10:29:07 +0200

    dawd

--- MVC/Views/Halloween/Index.php
+++ MVC/Views/Halloween/Index.php
@@ -31,7 +31,6 @@ class Index extends IView
             $div_image = new HTML('div', ['class' => 'contestant']);
             $div_image->setClasses(['d-flex', 'flex-column', 'align-items-center', 'm-3']);
 
-            // Imagen del disfraz
             $img = new HTML('img', ['src' => $contestant['image'], 'alt' => 'Disfraz Halloween']);
             $img->setStyle([
                 'width' => '200px',
@@ -40,18 +39,15 @@ class Index extends IView
                 'object-fit' => 'cover'
             ]);
 
-            // Botón de votar
             $voteButton = new HTML('button');
             $voteButton->setClasses(['btn', 'btn-vote', 'mt-3']);
             $voteButton->setText("Votar 🎃");
             $voteButton->setAttributes(['data-id' => $id]);
 
-            // Mostrar número de votos
             $votesCount = new HTML('span');
             $votesCount->setText("Votos: " . $contestant['votes']);
             $votesCount->setStyle(['margin-top' => '10px', 'color' => 'white']);
 
-            // Añadir elementos al contenedor
             $div_image->addElements([$img, $voteButton, $votesCount]);
             $this->div_gallery->addElement($div_image);
         }
